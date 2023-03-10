e9debd489a1e05ec26551fcfcca6393d3e09ea49
Nguyễn Ngọc Vĩnh <122826039+vinhDev3006@users.noreply.github.com>
2023-03-10 12:43:49 +0700

    Update and rename TurnOffAt9_45PM.ps1 to TurnOffAt9_15PM.ps1
    change the time

--- TurnOffAt9_15PM.ps1
+++ TurnOffAt9_15PM.ps1
@@ -1,7 +1,7 @@
 <# 
 This piece of program is created to remind me to
-turn off the computer at 10:30 PM everyday, if i don't shut it down
-before that time the computer will do itself at 11 PM.
+turn off the computer at 9:15 PM everyday, if i don't shut it down
+before that time the computer will do itself at 9:30 PM.
 
 Why? 🤔
 Go to sleep lol
@@ -29,14 +29,14 @@ $synthesizer = New-Object -TypeName System.Speech.Synthesis.SpeechSynthesizer
 
 # Get the current time and calculate the shutdown time
 $currentTime = Get-Date
-$shutdownTime = $currentTime.Date.AddHours(21).AddMinutes(45)
+$shutdownTime = $currentTime.Date.AddHours(21).AddMinutes(15)
 
 # Calculate the time until shutdown
 $timeUntilShutdown = $shutdownTime - $currentTime
 
 # Check if it's past the shutdown time
 if ($currentTime -gt $shutdownTime) {
-    $message = "It's past 9:45 PM. Your computer will shut down in 15 minutes."
+    $message = "It's past 9:15 PM. Your computer will shut down in 15 minutes. GO TO SLEEP NOW"
     Write-Host $message
     $synthesizer.Speak($message)
 
@@ -45,7 +45,7 @@ if ($currentTime -gt $shutdownTime) {
     Stop-Computer -Force
 
 } else {
-    $message = "Your computer will shut down at 10 PM."
+    $message = "Your computer will shut down NOW"
     Write-Host $message
     $synthesizer.Speak($message)
 
