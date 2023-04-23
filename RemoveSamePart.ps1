4510e3ba87548ab1da510bfbe2b89e24ebf94d83
Nguyễn Ngọc Vĩnh <122826039+vinhDev3006@users.noreply.github.com>
2023-04-23 23:22:18 +0700

    Update RemoveSamePart.ps1

--- RemoveSamePart.ps1
+++ RemoveSamePart.ps1
@@ -1,13 +1,12 @@
-
-# Ask the user for the folder path, file type, and string to replace
-$folder = Read-Host "Enter folder path:"
-$filetype = Read-Host "Enter file type (e.g. mp3):"
-$replace = Read-Host "Enter string to replace:"
+# Ask the user for the folder path, file type, and regex pattern
+$folder = Read-Host "Enter folder path"
+$filetype = Read-Host "Enter file type (e.g. mp3)"
+$regexPattern = Read-Host "Enter regex pattern (`hello` will remove hello, while -\w+-\d? will remove some string like -ae3se-4)"
 
 # Get all files of the specified type in the specified folder
 Get-ChildItem $folder\*.$filetype | ForEach-Object {
-    # Rename each file, removing the specified string
-    $newname = $_.Name.Replace($replace, "")
+    # Rename each file, removing the specified string using regex
+    $newname = $_.Name -replace $regexPattern, ""
     Rename-Item $_.FullName -NewName $newname
 }
 
