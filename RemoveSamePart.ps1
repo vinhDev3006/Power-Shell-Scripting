96c42aa5129c2412b9d8a82e144397d2a5ab2fd1
Nguyễn Ngọc Vĩnh <122826039+vinhDev3006@users.noreply.github.com>
2023-04-23 20:37:24 +0700

    Update RemoveSamePart.ps1

--- RemoveSamePart.ps1
+++ RemoveSamePart.ps1
@@ -2,8 +2,10 @@
 
 $fileType = Read-Host "Enter file type (e.g. mp3, wav)"
 $replaceString = Read-Host "Enter string to replace"
+$folderPath = Read-Host "Enter folder path (e.g. C:\Users\UserName\Music)"
 
-Get-ChildItem *.$fileType | ForEach-Object {
+Get-ChildItem -Path $folderPath -Filter *.$fileType | ForEach-Object {
     $newName = $_.Name.Replace($replaceString, "")
     Rename-Item $_.FullName $newName
 }
+
