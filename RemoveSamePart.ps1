2d48ed6c75261b130eec22cf365ab354be8836cb
Nguyễn Ngọc Vĩnh <122826039+vinhDev3006@users.noreply.github.com>
2023-04-23 20:51:10 +0700

    Update RemoveSamePart.ps1

--- RemoveSamePart.ps1
+++ RemoveSamePart.ps1
@@ -1,11 +1,15 @@
-# A simple script to remove the same part of the file
 
-$fileType = Read-Host "Enter file type (e.g. mp3, wav)"
-$replaceString = Read-Host "Enter string to replace"
-$folderPath = Read-Host "Enter folder path (e.g. C:\Users\UserName\Music)"
+# Ask the user for the folder path, file type, and string to replace
+$folder = Read-Host "Enter folder path:"
+$filetype = Read-Host "Enter file type (e.g. mp3):"
+$replace = Read-Host "Enter string to replace:"
 
-Get-ChildItem -Path $folderPath -Filter *.$fileType | ForEach-Object {
-    $newName = $_.Name.Replace($replaceString, "")
-    Rename-Item $_.FullName $newName
+# Get all files of the specified type in the specified folder
+Get-ChildItem $folder\*.$filetype | ForEach-Object {
+    # Rename each file, removing the specified string
+    $newname = $_.Name.Replace($replace, "")
+    Rename-Item $_.FullName -NewName $newname
 }
+
+
 
