1379ea3972a4bceddd0f76acb3114a8c81882a6e
Nguyễn Ngọc Vĩnh <122826039+vinhDev3006@users.noreply.github.com>
2023-11-04 23:46:18 +0700

    Update RemoveSamePart.ps1

--- RemoveSamePart.ps1
+++ RemoveSamePart.ps1
@@ -1,14 +1,22 @@
-# Ask the user for the folder path, file type, and regex pattern
 $folder = Read-Host "Enter folder path"
+$folder -replace " ",""
 $filetype = Read-Host "Enter file type (e.g. mp3)"
+
+# Initialize the regexPattern variable
 $regexPattern = Read-Host "Enter regex pattern (`hello` will remove hello, while -\w+-\d? will remove some string like -ae3se-4)"
 
-# Get all files of the specified type in the specified folder
-Get-ChildItem $folder\*.$filetype | ForEach-Object {
-    # Rename each file, removing the specified string using regex
-    $newname = $_.Name -replace $regexPattern, ""
-    Rename-Item $_.FullName -NewName $newname
+# Create a loop to repeatedly ask the user for the regexPattern
+while ($true -and $regexPattern) {
+
+    Get-ChildItem "$folder\*.$filetype" | ForEach-Object {
+        # Rename each file, removing the specified string using regex
+        $newname = $_.Name -replace $regexPattern, ""
+        Rename-Item $_.FullName -NewName $newname
+    }
+
+    $regexPattern = Read-Host "Next please, type "q" to quit?"
+    
+    if ($regexPattern -eq "q") {
+        break
+    }
 }
-
-
-
